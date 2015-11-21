15e756a8b0bfa412f5b356cd323907f8d4b75ed3
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2015-11-21 09:51:54 +0100

    name controller fix

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -4,9 +4,8 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class AppController extends Controller
 {
     /**
      * @Route("/", name="homepage")
